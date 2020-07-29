986466dd032577fbbb86022755b36243bdd96199
lgcolella <luigicolella.1997@gmail.com>
2020-07-30 00:36:14 +0200

    Fix spaziatura

--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,8 @@ module.exports = {
         port: 8080,
         headers: {
             "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
         }
     },
 
